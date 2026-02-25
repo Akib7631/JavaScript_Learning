0d8a762316d795ce871e6fd461f923ca4ff663cc
Akib7631 <razaakib0786@gmail.com>
2026-02-25 05:39:28 +0000

    Variables and datatypes

---
+++ 01_basic/var.js
@@ -0,0 +1,10 @@
+const AccountId = 12345;
+let email = "ak@5454";
+var AccountPas = "1232";
+accountCity = "Jaipur";
+
+email = "shad@21";
+AccountPas = "3434";
+accountCity = "Kolkata";
+console.log(AccountId);
+console.table([email,AccountPas,accountCity])
